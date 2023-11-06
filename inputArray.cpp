85d0427875782efa61858cccf0cf5c12c650d900
Zharko <zharko.aktobe@gmail.com>
2023-11-06 15:06:01 +0300

    good input array function

---
+++ inputArray.cpp
@@ -0,0 +1,19 @@
+#include "inputArray.hpp"
+#include <algorithm>
+#include <iostream>
+
+//Принимаем:  поток из которого считываем данные, указатель на начало массива,
+//            сколько элементов помещается, сколько элементов собираемся считать
+//Возвращаем: количество прочитанных элементов.
+size_t readArray::inputArray(std::istream & in, int * matrix, size_t s, size_t toRead)
+{
+  for (size_t i = 0; i < std::min(toread, s); ++i)
+  {
+    if (!(in >> matrix[i]))
+    {
+      read = i;
+      return in;
+    }
+  }
+  return std::min(toRead, s);
+}
---
+++ inputArray.hpp
@@ -0,0 +1,9 @@
+#ifndef INPUT_ARRAY_HPP
+#define INPUT_ARRAY_HPP
+#include <iosfwd>
+
+namespace readArray
+{
+  size_t inputArray(std::istream & in, int * matrix, size_t s, size_t toRead)
+}
+#endif
